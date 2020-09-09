11c9b3e891fffecf817d7c99e04fdce4da849cdf
Maksym Liannoi <liannoi444@gmail.com>
2020-09-09 10:24:54 +0300

    App specified for Hilt

--- app/src/main/java/org/itstep/liannoi/reactivesearch/presentation/ReactiveApplication.kt
+++ app/src/main/java/org/itstep/liannoi/reactivesearch/presentation/ReactiveApplication.kt
@@ -1,5 +1,7 @@
 package org.itstep.liannoi.reactivesearch.presentation
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class ReactiveApplication : Application()
