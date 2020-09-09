c8e7d1007796fd01807f740b824698e1b51aee13
Maksym Liannoi <liannoi444@gmail.com>
2020-09-09 12:57:33 +0300

    Added main layout

--- app/src/main/java/org/itstep/liannoi/reactivesearch/presentation/main/MainActivity.kt
+++ app/src/main/java/org/itstep/liannoi/reactivesearch/presentation/main/MainActivity.kt
@@ -2,8 +2,10 @@ package org.itstep.liannoi.reactivesearch.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import org.itstep.liannoi.reactivesearch.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/org/itstep/liannoi/reactivesearch/presentation/main/MainFragment.kt
+++ app/src/main/java/org/itstep/liannoi/reactivesearch/presentation/main/MainFragment.kt
@@ -1,5 +1,35 @@
 package org.itstep.liannoi.reactivesearch.presentation.main
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import org.itstep.liannoi.reactivesearch.databinding.FragmentMainBinding
 
-class MainFragment : Fragment()
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var viewDataBinding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewDataBinding = FragmentMainBinding.inflate(inflater, container, false).apply {
+            viewmodel = viewModel
+        }
+
+        return viewDataBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewDataBinding.lifecycleOwner = viewLifecycleOwner
+    }
+}
